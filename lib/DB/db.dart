040ae806e6ed301f4c3ae6a17a892d17a03d8344
duhui <dh151446@gmail.com>
2021-07-19 07:23:08 +0900

    add to insertDB method in db.dart

--- lib/DB/db.dart
+++ lib/DB/db.dart
@@ -1,5 +1,8 @@
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:todolist/DB/model.dart';
+
+final String tableName = 'Plan';
 
 class DbHelper{
   late Database db;
@@ -18,9 +21,17 @@ class DbHelper{
       path,
       version: 1,
       onCreate:(db, version) async{
-        await db.execute('CREATE TABLE Plan(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, date TEXT)');
+        await db.execute('CREATE TABLE $tableName(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, date TEXT)');
       }, onUpgrade: (db, oldVersion, newVersion){}
     ); 
   }
+
+  Future<Plan> insert(Plan plan) async{
+    final db = await database;
+    plan.id= await db.insert(tableName, plan.toMap());
+    return plan;
+  }
+
+  
   
 }
