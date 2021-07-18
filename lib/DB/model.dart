a4363092b24315e553c743ecad654aceba73a871
duhui <dh151446@gmail.com>
2021-07-18 18:50:43 +0900

    add methods of models

--- lib/DB/model.dart
+++ lib/DB/model.dart
@@ -1,17 +1,30 @@
+final String tableName = 'plan';              //https://pub.dev/packages/sqflite
+final String columnId = 'id';
+final String columnTitle = 'title';
+final String columnDate = 'date';
+
 class Plan{
-  final int id;
-  final String title;
-  final String date;
+  late final int id;
+  late final String title;
+  late final String date;
 
-  Plan({required this.id, required this.title, required this.date});          //각각 값이 다 들어갈 것이기에 required를 주었습니다.
-
+  Plan({required this.id, required this.title, required this.date});     
 
   Map<String, dynamic> toMap(){
-    return {
-      'id' : id,
-      'title' : title,
-      'date' : date,
+    var map =  {
+      columnTitle : title,
+      columnDate : date,
     };
+    if (id != null){
+      map[columnId] = id as String;
+    }
+    return map;
+  }
+
+  Plan.fromMap(Map<String, dynamic> map){
+    id = map[columnId];
+    title = map[columnTitle];
+    date = map[columnDate];
   }
 
 }
