deba72f65a56e3eecad6f0a1ba1381696d220326
duhui <dh151446@gmail.com>
2021-07-25 13:06:49 +0900

    add DBHelper

---
+++ lib/database/database_helper.dart
@@ -0,0 +1,79 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:todolist/model/model.dart';
+
+class DBHelper {
+  static const dbName = 'plan.db';
+  static const table = 'plan';
+  static const columnId = 'id';
+  static const columnTitle = 'title';
+  static const columnDate = 'date';
+
+  DBHelper._();
+  static final DBHelper instance = DBHelper._();
+  factory DBHelper() => instance;
+  static Database? database;
+
+  Future<Database?> get getDatabase async {
+    if (database != null) return database;
+
+    database = await initDB();
+    return database;
+  }
+
+  Future<Database> initDB() async {
+    var path = join(await getDatabasesPath(), dbName);
+
+    return await openDatabase(path, version: 1, onCreate: (db, version) async {
+      await db.execute('''
+          CREATE TABLE $table(
+            $columnId INTEGER PRIMARY KEY AUTOINCREMENT,
+            $columnTitle TEXT NOT NULL,
+            $columnDate TEXT NOT NULL,
+          )
+        ''');
+    }, onUpgrade: (db, oldVersion, newVersion) {});
+  }
+
+  //Create
+  Future createData(Plan plan) async {
+    final db = await database;
+    if(db != null){
+      var res = await db.insert(table, plan.toMap());
+      return res;
+    } else {
+      return Future.error('error');
+    }
+  }
+
+  //Read All
+  Future<List<Map<String, dynamic>>> getAllPlans() async {
+    final db = await database;
+    if(db != null){
+      return await db.query(table, orderBy: '$columnId DESC');
+    } else {
+      return Future.error('error');
+    }
+  }
+
+  //Delete
+  Future<Future<int>> deletePlan(int id) async {
+    final db = await database;
+    if(db != null){
+      var res = db.delete(table, where: '$columnId = ?', whereArgs: [id],);
+      return res;
+    } else {
+      return Future.error('error');
+    }
+  }
+
+  //Delete All
+  Future<void> clearPlans() async{
+    final db = await database;
+    if(db != null){
+      await db.rawDelete('DELETE FROM $table');
+    } else {
+      return Future.error('error');
+    }
+  }
+}
