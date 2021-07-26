e991ce957c6ac0f996172ee105375ff561feaae2
duhui <dh151446@gmail.com>
2021-07-27 06:44:38 +0900

    edit model

--- lib/model/model.dart
+++ lib/model/model.dart
@@ -2,10 +2,11 @@ class Plan {
   int? id;
   String title;
   String date;
+  bool ipt;
 
-  Plan({this.id, this.title = '', this.date = ''});
+  Plan({this.id, this.title = '', this.date = '', this.ipt = false});
 
   Map<String, dynamic> toMap() {
-    return {'id': id, 'title': title, 'date': date};
+    return {'id': id, 'title': title, 'date': date, 'ipt': ipt};
   }
 }
